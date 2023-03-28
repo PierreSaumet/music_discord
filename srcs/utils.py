50f0a0fd2d4f009cd7b1cceba73effc119efb401
PierreSaumet <psaumet.dvp@gmail.com>
2023-03-28 17:56:55 +0200

    [BASE] Adding embed for display music, can read only url from youtube

--- srcs/utils.py
+++ srcs/utils.py
@@ -1,3 +1,9 @@
+import discord
+
+from datetime import timedelta
+from pytube import Search
+
+
 class Colors:
     BLUE = '\033[94m'
     CYAN = '\033[96m'
@@ -7,3 +13,42 @@ class Colors:
     END = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
+def get_list_videos(message):
+    result_list = []
+    url = "https://www.youtube.com/watch?v="
+
+    s = Search(message)
+
+    for i in range(5):
+        tmp_dct = {}
+        tmp_dct["choice"] = i + 1
+        tmp_dct["title"] = s.results[i].title
+        tmp_dct["author"] = s.results[i].author
+        try:
+            tmp_dct["length"] = str(
+                timedelta(
+                    seconds=int(s.results[i].vid_info["videoDetails"]["lengthSeconds"])
+                )
+            )
+        except KeyError:
+            print({Colors.RED} + "Error in gettings 5 videos.", {Colors.END})
+            return 1
+        tmp_dct["url"] = url + s.results[i].vid_info["videoDetails"]["videoId"]
+        result_list.append(tmp_dct)
+
+    return result_list
+
+
+def create_embed(colour, title, author, url, description):
+    embed = discord.Embed(
+        colour=colour,
+        title=title,
+        description=description,
+    )
+
+    embed.set_author(name=author)
+    embed.set_image(url=url)
+
+    return embed
